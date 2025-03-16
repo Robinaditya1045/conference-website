6c5709774d504f826f8bbbe15e4c6341494feae1
Robinaditya1045 <mahamuniaditya07@gmail.com>
2025-03-17 02:11:49 +0530

    fix:scroll-to-top-button responsiveness

--- components/Buttons/scrollToTop.js
+++ components/Buttons/scrollToTop.js
@@ -27,12 +27,12 @@ const ScrollToTop = () => {
   }, []);
 
   return (
-<div className={`fixed bottom-6 right-6 z-50 transition-opacity duration-600 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
+    <div className={`fixed bottom-6 right-6 z-50 transition-opacity duration-600 ${isVisible ? 'opacity-100' : 'opacity-0'}`}>
       <button 
         onClick={scrollToTop} 
-        className="gradient-bg text-white m-5 p-3 rounded-full shadow-lg transition-transform duration-300 transform hover:scale-110"
+        className="gradient-bg text-white m-5 p-3 rounded-full shadow-lg transition-transform duration-300 transform hover:scale-110 flex justify-center items-center md:p-3 md:m-2 sm:p-2 sm:m-1"
       >
-        <ArrowUp size={24} />
+        <ArrowUp size={24} className="md:w-8 md:h-8 sm:w-6 sm:h-6" />
       </button>
     </div>
   );
